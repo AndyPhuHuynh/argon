17d1805dd910aad2740dbea41b3da677ce24ac4d
AndyPhuHuynh <andyphuhuynh4@gmail.com>
2025-09-21 23:49:26 -0400

    Removed old files

--- include/Argon/Config/ConfigHelpers.hpp
+++ include/Argon/Config/ConfigHelpers.hpp
@@ -3,9 +3,6 @@
 
 #include "Argon/Config/Config.hpp"
 #include "Argon/Config/OptionConfig.hpp"
-#include "Argon/Options/IOption.hpp"
-#include "Argon/Options/OptionCharBase.hpp"
-#include "Argon/Options/OptionIntegralBaseImpl.hpp"
 #include "Argon/Options/NewOptionTypeExtensions.hpp"
 
 namespace Argon::detail {
@@ -18,27 +15,6 @@ namespace Argon::detail {
         if constexpr (is_non_bool_number<ValueType>) {
             optionConfig.min = opt.getMin().has_value() ? opt.getMin().value() : parserConfig.getMin<ValueType>();
             optionConfig.max = opt.getMax().has_value() ? opt.getMax().value() : parserConfig.getMax<ValueType>();
-        }
-        if (const auto it = parserConfig.getDefaultConversions().find(std::type_index(typeid(ValueType)));
-            it != parserConfig.getDefaultConversions().end()) {
-            optionConfig.conversionFn = &it->second;
-        }
-        return optionConfig;
-    }
-
-    template <typename OptionType, typename ValueType>
-    auto getOptionConfig(const Config& parserConfig, const IOption *option) -> OptionConfig<ValueType> {
-        OptionConfig<ValueType> optionConfig;
-        if constexpr (is_numeric_char_type<ValueType>) {
-            if (const auto charOpt = dynamic_cast<const OptionCharBase<OptionType>*>(option); charOpt != nullptr) {
-                optionConfig.charMode = resolveCharMode(parserConfig.getDefaultCharMode(), charOpt->getCharMode());
-            }
-        }
-        if constexpr (is_non_bool_number<ValueType>) {
-            if (const auto numOpt = dynamic_cast<const OptionIntegralBase<ValueType>*>(option); numOpt != nullptr) {
-                optionConfig.min = numOpt->getMin().has_value() ? numOpt->getMin().value() : parserConfig.getMin<ValueType>();
-                optionConfig.max = numOpt->getMax().has_value() ? numOpt->getMax().value() : parserConfig.getMax<ValueType>();
-            }
         }
         if (const auto it = parserConfig.getDefaultConversions().find(std::type_index(typeid(ValueType)));
             it != parserConfig.getDefaultConversions().end()) {
